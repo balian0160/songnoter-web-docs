30fadeff6f1ec31057e7cdfb40e618fef21f30cf
Tom Brhel <Tom.Brhel@honeywell.com>
2025-04-25 09:34:18 +0200

    fix url in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://balian0160.github.io/songnoter-web-docs/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/songnoter-web-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
